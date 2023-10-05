105042faf8f62f5ca550db091a5be609fbd178ac
IJ Roy <141866152+ij-roy@users.noreply.github.com>
2023-10-05 20:13:54 +0530

    Update and rename question 1.py to question 01.py

--- question 01.py
+++ question 01.py
@@ -4,7 +4,10 @@ Created on Fri Sep 22 15:02:07 2023
 
 @author: LAB
 """
-
+'''
+1. Find the count of vowels and consonants, words in a string using a for loop. Assume there is a
+single space between consecutive words in a sentence.
+'''
 # que 1 find the count of vowels and consoonants, words in a string using a for loop.assume there is a single space between consecutive words in a sentence
 V = 0
 i = 0
